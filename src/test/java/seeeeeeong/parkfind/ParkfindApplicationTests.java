f2ac936a6e0ebef5aa8f804a4f68e41fc84e5ca5
seeeeeeong <lsinsung@gmail.com>
2024-05-28 04:27:05 +0900

    no message

---
+++ src/test/java/seeeeeeong/parkfind/ParkfindApplicationTests.java
@@ -0,0 +1,13 @@
+package seeeeeeong.parkfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkfindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/seeeeeeong/parkfind/TestParkfindApplication.java
@@ -0,0 +1,23 @@
+package seeeeeeong.parkfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestParkfindApplication {
+
+	@Bean
+	@ServiceConnection
+	MariaDBContainer<?> mariaDbContainer() {
+		return new MariaDBContainer<>(DockerImageName.parse("mariadb:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(ParkfindApplication::main).with(TestParkfindApplication.class).run(args);
+	}
+
+}
