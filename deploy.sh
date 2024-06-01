63d458299f19fbdd0cad48c162f8ccc3ddc10e55
seeeeeeong <lsinsung@gmail.com>
2024-06-02 05:59:26 +0900

    fix: docker-compose

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-IS_GREEN=$(docker ps | grep parkfind-green) # 현재 실행중인 App이 green인지 확인합니다.
+IS_GREEN=$(docker-compose -f /path/to/docker-compose.yml ps green | grep Up) # 현재 실행중인 App이 green인지 확인합니다.
 DEFAULT_CONF="/etc/nginx/nginx.conf"
 
 if [ -z "$IS_GREEN" ]; then # green이 실행 중이지 않다면
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  docker-compose pull parkfind-green # green 이미지를 내려받습니다.
+  docker-compose -f /path/to/docker-compose-green.yml pull # green 이미지를 내려받습니다.
 
   echo "2. green container up"
-  docker-compose up -d parkfind-green # green 컨테이너 실행
+  docker-compose -f /path/to/docker-compose-green.yml up -d # green 컨테이너 실행
 
   # green 컨테이너 상태 확인
   while true; do
     echo "Checking if green container is running..."
-    GREEN_STATUS=$(docker ps | grep parkfind-green)
+    GREEN_STATUS=$(docker-compose -f /path/to/docker-compose-green.yml ps -q green)
     if [ -n "$GREEN_STATUS" ]; then
       echo "Green container is running. Proceeding with health check..."
       break
@@ -40,44 +40,23 @@ if [ -z "$IS_GREEN" ]; then # green이 실행 중이지 않다면
   sudo nginx -s reload
 
   echo "5. blue container down"
-  docker-compose stop parkfind-blue
+  docker-compose -f /path/to/docker-compose-blue.yml stop
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose pull parkfind-blue
+  docker-compose -f /path/to/docker-compose-blue.yml pull
 
   echo "2. blue container up"
-  docker-compose up -d parkfind-blue
+  docker-compose -f /path/to/docker-compose-blue.yml up -d
 
   # blue 컨테이너 상태 확인
   while true; do
     echo "Checking if blue container is running..."
-    BLUE_STATUS=$(docker ps | grep parkfind-blue)
+    BLUE_STATUS=$(docker-compose -f /path/to/docker-compose-blue.yml ps -q blue)
     if [ -n "$BLUE_STATUS" ]; then
       echo "Blue container is running. Proceeding with health check..."
       break
     else
       echo "Blue container is not running yet. Waiting..."
-      sleep 3
-    fi
-  done
-
-  while true; do
-    echo "3. blue health check..."
-    sleep 3
-    REQUEST=$(curl -s http://3.37.167.45:8080) # blue로 request
-
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-      echo "health check success"
-      break
-    fi
-  done
-
-  echo "4. reload nginx"
-  sudo cp /nginx/nginx.blue.conf $DEFAULT_CONF
-  sudo nginx -s reload
-
-  echo "5. green container down"
-  docker-compose stop parkfind-green
-fi
+      sleep
